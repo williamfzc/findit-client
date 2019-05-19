5d535160473634c6e7a59996e14e339c947de898
williamfzc <178894043@qq.com>
2019-05-19 17:41:54 +0800

    add: test case

---
+++ tests/test_client.py
@@ -0,0 +1,25 @@
+import pytest
+import subprocess
+import time
+
+from findit_client import FindItClient
+
+find_it_client = FindItClient()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def life_time():
+    server_process = subprocess.Popen('python3 -m findit.server --dir tests/pics', shell=True)
+    time.sleep(3)
+    yield
+    server_process.terminate()
+    server_process.kill()
+
+
+def test_heartbeat():
+    assert find_it_client.heartbeat()
+
+
+def test_analyse_with_path():
+    result = find_it_client.analyse_with_path('tests/pics/screen.png', 'wechat_logo.png')
+    assert 'data' in result
