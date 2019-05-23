00d175aa1b0e9e4fe5c90c53f9798b927e74d54f
williamfzc <178894043@qq.com>
2019-05-23 11:12:09 +0800

    update: more test case

--- tests/test_client.py
+++ tests/test_client.py
@@ -22,4 +22,11 @@ def test_heartbeat():
 
 def test_analyse_with_path():
     result = find_it_client.analyse_with_path('tests/pics/screen.png', 'wechat_logo.png')
-    assert 'data' in result
+    assert 'request' in result
+    assert 'response' in result
+    assert 'msg' in result
+    assert 'status' in result
+
+    assert 'extras' in result['request']
+    assert 'template_name' in result['request']
+    assert 'data' in result['response']
--- tests/test_standard.py
+++ tests/test_standard.py
@@ -48,7 +48,8 @@ def test_analyse_with_object():
 
 def test_analyse_with_extras():
     result = find_it_client.analyse_with_path(
-        'tests/pics/screen.png', 'wechat_logo.png', a='123', b='456', need_log=True)
+        'tests/pics/screen.png', 'wechat_logo.png',
+        a='123', b='456', pro_mode=True, engine_template_scale=(1, 4, 10))
 
     assert 'request' in result
     assert 'response' in result
@@ -58,3 +59,4 @@ def test_analyse_with_extras():
     assert 'extras' in result['request']
     assert 'a' in result['request']['extras']
     assert 'b' in result['request']['extras']
+    assert 'engine_template_scale' in result['request']['extras']
