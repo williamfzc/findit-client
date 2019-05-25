5d2964a851bc384d284549cb4691c18edfe7cc6a
williamfzc <178894043@qq.com>
2019-05-25 23:29:21 +0800

    release: 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='findit_client',
-    version='0.1.1',
+    version='0.1.2',
     description='client for findit, with no opencv needed',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
